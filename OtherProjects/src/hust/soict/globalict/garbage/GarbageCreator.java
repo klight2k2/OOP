8fd7f0c7174e16e7ae1e6642c82335becf5b49da
quang.phamminh2 <quang.phamminh2@vti.com.vn>
2022-11-25 17:26:55 +0700

    add garbage

---
+++ OtherProjects/src/hust/soict/globalict/garbage/GarbageCreator.java
@@ -0,0 +1,29 @@
+package hust.soict.globalict.garbage;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+public class GarbageCreator {
+	public static void main(String[] args) throws IOException {
+		String filename="test.txt";
+		byte[] inputBytes= {0};
+		long startTime,endTime;
+		
+		try {
+			inputBytes=Files.readAllBytes(Paths.get(filename));
+        } catch (IOException x) {
+            System.err.format("IOException: %s%n", x);
+        }
+		startTime=System.currentTimeMillis();
+		String outputString="";
+		
+		for(byte b: inputBytes) {
+			outputString+=(char)b;
+		}
+		endTime=System.currentTimeMillis();
+		System.out.println(endTime-startTime);
+	}
+	
+	
+
+}
